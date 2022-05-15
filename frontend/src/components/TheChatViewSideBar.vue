73280fe9075d91e9a6871683c65ec8b73e7de3e2
canneth <CHJrWithaC@gmail.com>
2022-05-15 23:08:31 +0800

    feat: First steps on basic skeleton of ChatView

---
+++ frontend/src/components/TheChatViewSideBar.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+
+import CommonSearchBar from '@/components/common/CommonSearchBar.vue';
+import useRootStore from '@/composables/useRootStore';
+import { computed } from '@vue/reactivity';
+
+const store = useRootStore();
+
+const displayName = computed(() => {
+  return store.value.loggedInUser?.name.split(' ')[0];
+});
+
+</script>
+
+<template>
+  <div :class="$style.overallContainer">
+    <header :class="$style.header">
+      <div :class="$style.profileBar">
+        <div :class="$style.avatarContainer">
+          <img
+            :class="$style.avatarImg"
+            :src="store.loggedInUser?.avatarSrc ?? 'placeholder-avatar.png'" />
+        </div>
+        <p :class="$style.displayName">
+          {{ displayName }}
+        </p>
+      </div>
+      <CommonSearchBar />
+    </header>
+  </div>
+</template>
+
+<style module>
+.overallContainer {
+  position: relative;
+  height: 100%;
+  width: 300px;
+
+  border: 1px solid;
+}
+.header {
+  position: relative;
+  height: 100px;
+  width: 100%;
+  background-color: var(--color-primary-base);
+
+  border: 1px solid;
+}
+.profileBar {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  padding: 10px 16px;
+}
+.avatarContainer {
+  position: relative;
+  border-radius: 50%;
+  overflow: clip;
+  width: 50px;
+  aspect-ratio: 1 / 1;
+}
+.avatarImg {
+  width: 100%;
+  height: auto;
+}
+.displayName {
+  font-weight: bold;
+  color: var(--color-grey-white);
+}
+</style>
--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -2,26 +2,31 @@
 
 import { useRouter } from 'vue-router';
 import useRootStore from '@/composables/useRootStore';
+import TheChatViewSideBar from '@/components/TheChatViewSideBar.vue';
 
 const router = useRouter();
 const store = useRootStore();
 
-if (!store.value.loggedInUser) router.push('/login');
+// if (!store.value.loggedInUser) router.push('/login');
 
 </script>
 
 <template>
   <div :class="$style.overallContainer">
-    <p>I am the ChatView!</p>
+    <TheChatViewSideBar :class="$style.sideBar" />
   </div>
 </template>
 
 <style module>
 .overallContainer {
   position: relative;
-  display: grid;
-  place-items: center;
   width: 100%;
   height: 100vh;
+  display: flex;
+}
+.sideBar {
+  position: relative;
+  height: 100%;
+  width: clamp(350px, 30vw, 500px);
 }
 </style>
