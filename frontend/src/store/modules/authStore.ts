9a299a3846d71fce3caa7d28ddb05b1d8f994b3c
canneth <CHJrWithaC@gmail.com>
2022-05-15 18:31:05 +0800

    refactor: Support direct access to properties in store

--- frontend/src/store/modules/authStore.ts
+++ frontend/src/store/modules/authStore.ts
@@ -2,13 +2,10 @@
 import type User from '@/models/User';
 
 const authStore = {
-  _loggedInUser: null as User | null,
+  loggedInUser: null as User | null,
 
-  get loggedInUser() {
-    return this._loggedInUser;
-  },
   setLoggedInUser(user: User | null) {
-    this._loggedInUser = user;
+    this.loggedInUser = user;
   }
 };
 
