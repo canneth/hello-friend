4c00218fa3bd5faa8a6b8e65c14e4615ca48b2f1
canneth <CHJrWithaC@gmail.com>
2022-05-15 17:01:54 +0800

    feat: Flesh out HomeView

---
+++ frontend/src/components/common/CommonButton.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+
+import { useRouter } from 'vue-router';
+
+const props = defineProps<{
+  text: string;
+  url: string;
+  noBackground?: boolean;
+}>()
+
+const router = useRouter();
+
+function clickHandler(e: MouseEvent) {
+  e.preventDefault();
+  router.push(props.url);
+}
+
+</script>
+
+<template>
+  <button
+    :class="`overall-container ${noBackground ? 'no-background' : null}`"
+    @click="clickHandler">
+    <p class="text">{{ text }}</p>
+  </button>
+</template>
+
+<style scoped>
+.overall-container {
+  position: relative;
+  display: grid;
+  place-items: center;
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: var(--color-primary-dark);
+  border: 2px solid transparent;
+  transition:
+    background-color 100ms ease-out,
+    border-color 100ms ease-out;
+}
+.text {
+  font-weight: bold;
+  font-size: var(--font-size-regular);
+  color: var(--color-grey-white);
+  transition: color 100ms ease-out;
+}
+
+.overall-container.no-background {
+  background-color: var(--color-grey-white);
+}
+.overall-container.no-background > .text {
+  color: var(--color-primary-dark);
+}
+
+.overall-container:hover {
+  cursor: pointer;
+  background-color: white;
+  border-color: var(--color-grey-black);
+}
+.overall-container:hover > .text {
+  color: var(--color-grey-black);
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,6 +2,8 @@
 
 import TheNavBar from '@/components/TheNavBar.vue';
 import useRootStore from '@/store/useRootStore';
+import SvgLogo from '../components/svg/SvgLogo.vue';
+import CommonButton from '../components/common/CommonButton.vue';
 
 const store = useRootStore();
 
@@ -13,7 +15,19 @@ const store = useRootStore();
       <TheNavBar />
     </header>
     <main class="page-content-container">
-      <p>I am the HomeView!</p>
+      <div class="hero-container">
+        <SvgLogo class="logo-svg" />
+        <div class="hero-content-container">
+          <div class="hero-title-container">
+            <h2>Welcome to</h2>
+            <h1>HelloFriend!</h1>
+          </div>
+          <menu class="button-bar">
+            <CommonButton text="Get Chatting!" url="/chat" />
+            <CommonButton text="About" noBackground url="/about" />
+          </menu>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -25,5 +39,50 @@ const store = useRootStore();
   place-items: center;
   width: 100%;
   height: 100vh;
+}
+.hero-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+.logo-svg {
+  width: clamp(220px, 40vw, 500px);
+  height: auto;
+}
+.hero-content-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+.hero-title-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+.hero-title-container > h2 {
+  font-size: clamp(20px, 3vw, 40px);
+}
+.hero-title-container > h1 {
+  font-size: clamp(50px, 8vw, 100px);
+}
+.button-bar {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+@media (max-width: 800px) {
+  .hero-container {
+    flex-direction: column;
+  }
+  .hero-title-container {
+    align-items: center;
+  }
+  .button-bar {
+    justify-content: center;
+  }
 }
 </style>
