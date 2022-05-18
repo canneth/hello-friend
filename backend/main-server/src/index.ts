6b6e2030ff9d90a1ffc8fe6e432e6108d02f609c
canneth <CHJrWithaC@gmail.com>
2022-05-18 09:05:45 +0800

    dev: Basic project setup for main server

---
+++ backend/main-server/src/index.ts
@@ -0,0 +1,11 @@
+
+import express from 'express';
+
+const app = express();
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Main server listening on port ${port}`);
+});
