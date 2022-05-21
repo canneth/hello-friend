6c158acb34cfbc52484f3a8c071f6a83e8298d0e
canneth <CHJrWithaC@gmail.com>
2022-05-21 13:36:22 +0800

    dev: Add more functions to users service

---
+++ backend/main-server/src/routes/api/users/users.service.test.ts
@@ -0,0 +1,114 @@
+
+import User from '@src/database/schemas/User';
+import knexClient from '@src/database/client';
+import { getUserByEmail, getUserById } from './users.service';
+
+describe('getUserById(user)', () => {
+  it('if the corresponding user exists in the database, returns the user', async () => {
+    /* Setup */
+    const testUser: Partial<User> = {
+      userId: 'fc72363c-4d5e-4884-9241-545e911049e8',
+      email: 'some@email.com',
+      handle: 'some-handle',
+      password: 'some-password',
+      name: 'Some Name',
+      avatarSrc: 'some-url'
+    }
+    await knexClient<User>('User').delete().where(testUser);
+    await knexClient<User>('User').insert(testUser);
+
+    /* Execute */
+    const retrievedUser = await getUserById(testUser.userId!);
+    delete retrievedUser?.dtmCreated;
+
+    /* Cleanup */
+    await knexClient<User>('User').delete().where(testUser);
+
+    /* Expect */
+    expect(retrievedUser).toEqual(testUser);
+  });
+  it('if the user does not exist in the database, returns null', async () => {
+    /* Setup */
+    const testUser: Partial<User> = {
+      userId: 'fc72363c-4d5e-4884-9241-545e911049e8',
+      email: 'some@email.com',
+      handle: 'some-handle',
+      password: 'some-password',
+      name: 'Some Name',
+      avatarSrc: 'some-url'
+    }
+    await knexClient<User>('User').delete().where(testUser);
+
+    /* Execute */
+    const retrievedUser = await getUserById(testUser.userId!);
+
+    /* Expect */
+    expect(retrievedUser).toStrictEqual(null);
+  });
+});
+
+describe('getUserByEmail(email)', () => {
+  it('if the corresponding user exists in the database, returns user', async () => {
+    /* Setup */
+    const testUser: Partial<User> = {
+      userId: 'fc72363c-4d5e-4884-9241-545e911049e8',
+      email: 'some@email.com',
+      handle: 'some-handle',
+      password: 'some-password',
+      name: 'Some Name',
+      avatarSrc: 'some-url'
+    }
+    await knexClient<User>('User').delete().where(testUser);
+    await knexClient<User>('User').insert(testUser);
+
+    /* Execute */
+    const retrievedUser = await getUserByEmail(testUser.email!);
+    delete retrievedUser?.dtmCreated;
+
+    /* Cleanup */
+    await knexClient<User>('User').delete('*').where(testUser);
+
+    /* Expect */
+    expect(retrievedUser).toEqual(testUser);
+  });
+  it('if the user does not exist in the database, returns null', async () => {
+    /* Setup */
+    const testUser: Partial<User> = {
+      userId: 'fc72363c-4d5e-4884-9241-545e911049e8',
+      email: 'some@email.com',
+      handle: 'some-handle',
+      password: 'some-password',
+      name: 'Some Name',
+      avatarSrc: 'some-url'
+    }
+    await knexClient<User>('User').delete().where(testUser);
+
+    /* Execute */
+    const retrievedUser = await getUserByEmail(testUser.email!);
+
+    /* Expect */
+    expect(retrievedUser).toStrictEqual(null);
+  });
+});
+
+describe('addNewUser(user)', () => {
+  it('adds the user to the database', async () => {
+    /* Setup */
+    const testUser: Partial<User> = {
+      userId: 'fc72363c-4d5e-4884-9241-545e911049e8',
+      email: 'some@email.com',
+      handle: 'some-handle',
+      password: 'some-password',
+      name: 'Some Name',
+      avatarSrc: 'some-url'
+    }
+    await knexClient<User>('User').delete().where(testUser);
+
+    /* Execute */
+    const queryResult = await knexClient<User>('User').insert(testUser).returning(Object.keys(testUser));
+    const insertedUser = queryResult[0];
+
+    /* Expect */
+    expect(insertedUser).toEqual(testUser);
+  });
+});
--- backend/main-server/src/routes/api/users/users.service.ts
+++ backend/main-server/src/routes/api/users/users.service.ts
@@ -1,12 +1,29 @@
 
+import { v4 as uuid } from 'uuid';
 import knexClient from '@src/database/client';
 import User from '@root/src/database/schemas/User';
 
-export async function getUser(userId: User['userId']) {
-  const queryResult = await (
-    knexClient<User>('User')
-      .select('*')
-      .where('userId', userId)
-  );
-  return queryResult[0];
+
+export async function getUserById(userId: User['userId']) {
+  const queryResult = await knexClient<User>('User').select('*').where('userId', userId);
+  const user = queryResult.length > 0 ? queryResult[0] : null;
+  return user;
+}
+
+export async function getUserByEmail(email: User['email']) {
+  const queryResult = await knexClient<User>('User').select('*').where('email', email);
+  const user = queryResult.length > 0 ? queryResult[0] : null;
+  return user;
+}
+
+export async function addNewUser(user: Partial<User> & { email: User['email'], password: User['password'] }) {
+  const userToAdd: User = {
+    userId: user.userId ?? uuid(),
+    email: user.email,
+    handle: user.handle ?? user.email.split('@')[0],
+    password: user.password,
+    name: user.name ?? user.email.split('@')[0],
+    avatarSrc: user.avatarSrc ?? null
+  }
+  await knexClient<User>('User').insert(userToAdd);
 }
