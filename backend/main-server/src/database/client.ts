1c6f80e5b81aac786651959f50036c52af8a8303
canneth <CHJrWithaC@gmail.com>
2022-05-21 17:22:43 +0800

    refactor: Separate test and dev databases

--- backend/main-server/src/database/client.ts
+++ backend/main-server/src/database/client.ts
@@ -4,20 +4,36 @@ import knex from 'knex';
 const knexClient = knex({
   client: 'pg',
   connection: (
-    process.env.NODE_ENV === 'development' || 'test'
-      ? {
-        host: 'localhost',
-        user: 'postgres',
-        port: 5432,
-        password: process.env.PG_DB_PASSWORD,
-        database: 'postgres'
-      }
-      : {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false
+    (() => {
+      switch (process.env.NODE_ENV) {
+        case 'development': {
+          return {
+            host: 'localhost',
+            user: 'postgres',
+            port: 5432,
+            password: process.env.PG_DEV_DB_PASSWORD,
+            database: 'dev'
+          };
+        }
+        case 'test': {
+          return {
+            host: 'localhost',
+            user: 'postgres',
+            port: 5432,
+            password: process.env.PG_TEST_DB_PASSWORD,
+            database: 'test'
+          };
+        }
+        default: {
+          return {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false
+            }
+          };
         }
       }
+    })()
   )
 });
 
