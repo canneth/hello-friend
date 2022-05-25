866a3af84aade39346705e2bd627b61cd8693c6d
canneth <CHJrWithaC@gmail.com>
2022-05-25 23:08:00 +0800

    dev: Remove unecessary console log

--- backend/main-server/src/import-aliases/config.ts
+++ backend/main-server/src/import-aliases/config.ts
@@ -6,8 +6,6 @@ const rootPath = path.resolve(__dirname, '..', '..');
 const rootPathDev = path.resolve(rootPath, 'src');
 const rootPathProd = path.resolve(rootPath, 'build');
 
-console.log(process.env.NODE_ENV);
-
 moduleAlias.addAliases({
   '@src': process.env.NODE_ENV === 'development' ? rootPathDev : rootPathProd
 });
