647c66ea4544e4c3e49a4c4875cfb4c3e99d2a3e
canneth <CHJrWithaC@gmail.com>
2022-05-26 18:13:57 +0800

    feat: Implement backend interaction when posting

--- frontend/src/components/TheChatWindowInputBar.vue
+++ frontend/src/components/TheChatWindowInputBar.vue
@@ -1,17 +1,45 @@
 <script setup lang="ts">
 
-// const props = defineProps<{
+import useUIStore from '@/composables/useUIStore';
+import useUserStore from '@/composables/useUserStore';
+import backendAxios from '@/globals/configuredAxios';
+import type ChatMessage from '@/schemas/ChatMessage';
+import { computed, ref } from 'vue';
 
-// }>()
+const uiStore = useUIStore();
+const userStore = useUserStore();
+
+const selfUserId = computed(() => userStore.value.user?.userId);
+const chatId = computed(() => uiStore.value.activeChat?.chatId);
+
+const messageContent = ref('');
+
+async function clickHandlerPostButton(e: MouseEvent) {
+  e.preventDefault();
+  if (!selfUserId.value) throw new Error('Cannot send a message without being logged in!');
+  if (!chatId.value) throw new Error('Cannot send a message without an activeChat in the uiStore!');
+  const content = messageContent.value;
+  if (!content) return;
+  const chatMessage: Pick<ChatMessage, 'userId' | 'chatId' | 'content'> = {
+    userId: selfUserId.value,
+    chatId: chatId.value,
+    content
+  };
+  try {
+    await backendAxios.post(`/api/chatMessages`, chatMessage);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 </script>
 
 <template>
   <div :class="$style.overallContainer">
     <div :class="$style.inputContainer">
-      <input :class="$style.input" type="text" placeholder="Say something!" />
+      <input :class="$style.input" type="text" placeholder="Say something!" v-model="messageContent" />
     </div>
-    <button :class="$style.postButton">
+    <button :class="$style.postButton" @click="clickHandlerPostButton">
       <span class="iconify" data-icon="fluent:send-20-filled" data-width="100%" data-height="auto"></span>
     </button>
   </div>
